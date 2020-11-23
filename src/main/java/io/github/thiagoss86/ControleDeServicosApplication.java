0fc70dd445989f3be37777f7a606a5567d31d40d
Thiago Siqueira dos Santos <thiago.santos@spread.com.br>
2020-11-23 09:02:25 -0300

    Commit inicial com a criação base do projeto

---
+++ src/main/java/io/github/thiagoss86/ControleDeServicosApplication.java
@@ -0,0 +1,12 @@
+package io.github.thiagoss86;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeServicosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControleDeServicosApplication.class, args);
+    }
+}
